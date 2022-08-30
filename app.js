e596508ec602dabda6d58c65687f1d38fad87a64
Datagirl <datagirlcmr@gmail.com>
2022-08-30 06:11:47 +0100

    format code using prettier

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 var companies = [
-    {
-     rank: 1,
-     name: 'Google', 
-     salary: '$130K', 
-     logo: "https://assets.codepen.io/6060109/salaries-icon-google.png"
-    },
-    {
-      rank: 2,
-      name: 'Facebook', 
-      salary: '$125K', 
-      logo: "https://assets.codepen.io/6060109/salaries-icon-facebook.png"
-    },
-    {
-      rank: 3,
-      name: 'LinkedIn', 
-      salary: '$110K', 
-      logo: "https://assets.codepen.io/6060109/salaries-icon-linkedin.png"
-    },
-    {
-      rank: 4,
-      name: 'Uber', 
-      salary: '$110K', 
-      logo: "https://assets.codepen.io/6060109/salaries-icon-uber.png"
-    },
-    {
-      rank: 5,
-      name: 'Amazon', 
-      salary: '$105K', 
-      logo: "https://assets.codepen.io/6060109/salaries-icon-amazon.png"
-    },
-  ];
-  
-  var companiesElement = document.querySelector('.companies');
-  
-  for (let company of companies) {
-    var newCompanyElement = `
+  {
+    rank: 1,
+    name: "Google",
+    salary: "$130K",
+    logo: "https://assets.codepen.io/6060109/salaries-icon-google.png",
+  },
+  {
+    rank: 2,
+    name: "Facebook",
+    salary: "$125K",
+    logo: "https://assets.codepen.io/6060109/salaries-icon-facebook.png",
+  },
+  {
+    rank: 3,
+    name: "LinkedIn",
+    salary: "$110K",
+    logo: "https://assets.codepen.io/6060109/salaries-icon-linkedin.png",
+  },
+  {
+    rank: 4,
+    name: "Uber",
+    salary: "$110K",
+    logo: "https://assets.codepen.io/6060109/salaries-icon-uber.png",
+  },
+  {
+    rank: 5,
+    name: "Amazon",
+    salary: "$105K",
+    logo: "https://assets.codepen.io/6060109/salaries-icon-amazon.png",
+  },
+];
+
+var companiesElement = document.querySelector(".companies");
+
+for (let company of companies) {
+  var newCompanyElement = `
       <li class="company">
         <h3>${company.rank}.</h3>
         <img src=${company.logo} >
@@ -42,5 +42,5 @@ var companies = [
         <h3>${company.salary}.</h3>
       </li>
       `;
-    companiesElement.innerHTML += newCompanyElement;
-  }
+  companiesElement.innerHTML += newCompanyElement;
+}
